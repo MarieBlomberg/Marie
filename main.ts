59ca531eeb68b7ee477daf4a10b19573b31d6bad
MarieBlomberg <108996421+MarieBlomberg@users.noreply.github.com>
2022-07-09 20:06:53 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,54 @@
+basic.showString("Hall ale sammen")
+basic.showIcon(IconNames.Heart)
+music.playMelody("C5 B A G F E D C ", 120)
+for (let index = 0; index < 2; index++) {
+    basic.showIcon(IconNames.Heart)
+    music.playMelody("C5 B A G F E D C ", 120)
+}
+basic.showIcon(IconNames.Heart)
+music.playMelody("C5 B A G F E D C ", 235)
+basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.Heart)
+music.playMelody("C5 B A G F E D C ", 120)
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    # . . . #
+    # # . # #
+    # . # . #
+    # . . . #
+    # . . . #
+    `)
+basic.showLeds(`
+    . . . . .
+    # # # # .
+    # . . # .
+    # # # # .
+    # . . # .
+    `)
+basic.showLeds(`
+    . . . . .
+    # # # # .
+    # . . # .
+    # # # # .
+    # . . # .
+    `)
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    # # # . .
+    # . . . .
+    # # # . .
+    # . . . .
+    # # # . .
+    `)
+basic.showIcon(IconNames.Heart)
+music.playMelody("C C5 B A G A B C ", 120)
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
